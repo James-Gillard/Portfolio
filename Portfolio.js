f0401485a579de1c72fab6ffde19072320ce671e
Jame Gillard <jame-gillard@hotmail.com>
2023-01-10 14:34:55 +0000

    day 3 update

--- Portfolio.js
+++ Portfolio.js
@@ -1,6 +1,24 @@
-function enlargeLogo(logo) {
-    document.getElementById("html-logo").innerHTML {
-        logo.style.width = '400px'
-    }
-    addEventListener('mouseover', enlargeLogo)
+
+function showProjectOne() {
+    document.getElementById('hidden-project-one').style.display = 'block'
+    document.getElementById('project-one-title').style.textDecoration = 'underline'
 }
+addEventListener('click', showProjectOne)
+
+function hideProjectOne() {
+    document.getElementById('hidden-project-one').style.display = 'none'
+    document.getElementById('project-one-title').style.textDecoration = 'none'
+}
+addEventListener('mouseout', hideProjectOne)
+
+function showProjectTwo() {
+    document.getElementById('hidden-project-two').style.display = 'block'
+    document.getElementById('project-two-title').style.textDecoration = 'underline' 
+}
+addEventListener('click', showProjectTwo)
+
+function hideProjectTwo() {
+    document.getElementById('hidden-project-two').style.display = 'none' 
+    document.getElementById('project-two-title').style.textDecoration = 'none'
+}
+addEventListener('mouseout', hideProjectTwo)
